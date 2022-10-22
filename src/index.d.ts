18ea6fb3cbfe79be7c7d4947fbbc235f4688c2d3
henriq20 <henrique-ferreira20@hotmail.com>
2022-10-21 22:13:56 -0300

    feat: add types

---
+++ src/index.d.ts
@@ -0,0 +1,4 @@
+declare function dedent(fragments: TemplateStringsArray | string, ...args: any[]) : string;
+declare function dedent(join: string, fragments: TemplateStringsArray | string, ...args: any[]) : string;
+
+export default dedent;
