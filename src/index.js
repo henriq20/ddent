7de76c36037a6da914deefbaa7a30afc91fec7f3
henriq20 <henrique-ferreira20@hotmail.com>
2022-10-22 12:11:07 -0300

    fix: eslint script and format code

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 export default function dedent(fragments, ...args) {
-    let join = '\n';
-    if (arguments.length === 2 && typeof fragments === 'string') {
-        join = fragments;
-        fragments = args.shift();
-    }
+	let join = '\n';
+	if (arguments.length === 2 && typeof fragments === 'string') {
+		join = fragments;
+		fragments = args.shift();
+	}
 
-    fragments = typeof fragments === 'string' ? [ fragments ] : fragments;
+	fragments = typeof fragments === 'string' ? [ fragments ] : fragments;
 
-    let result = fragments[0];
-    for (let i = 0; i < args.length; i++) {
-        result += args[i] + fragments[i + 1];
-    }
-    result = result.trim();
+	let result = fragments[0];
+	for (let i = 0; i < args.length; i++) {
+		result += args[i] + fragments[i + 1];
+	}
+	result = result.trim();
 
-    let indentation = Math.min(...result.match(/\n([ \t])+/g)?.map(m => m.length) || [ 0 ]);
-    indentation -= 1; // Ignore line break length
+	let indentation = Math.min(...result.match(/\n([ \t])+/g)?.map(m => m.length) || [ 0 ]);
+	indentation -= 1; // Ignore line break length
 
-    return result.replace(new RegExp(`\n[ \t]{${ indentation }}`, 'g'), join);
+	return result.replace(new RegExp(`\n[ \t]{${ indentation }}`, 'g'), join);
 }
