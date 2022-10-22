7af61b90e56a914836f84ec375ea6a915cc64fee
henriq20 <henrique-ferreira20@hotmail.com>
2022-10-21 22:23:31 -0300

    docs: add comments to function

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,11 @@
+/**
+ * Strips indentation from the given string.
+ */
 declare function dedent(fragments: TemplateStringsArray | string, ...args: any[]) : string;
+
+/**
+ * Strips indentation from the given string and joins it with the specified character.
+ */
 declare function dedent(join: string, fragments: TemplateStringsArray | string, ...args: any[]) : string;
 
 export default dedent;
